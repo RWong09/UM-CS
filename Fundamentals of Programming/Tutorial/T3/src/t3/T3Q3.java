6bb949e96512901611fb57e7bb74228c4c40be4f
RWong09 <147460891+RWong09@users.noreply.github.com>
2024-08-23 01:33:44 +0800

    Add files via upload

---
+++ Fundamentals of Programming/Tutorial/T3/src/t3/T3Q3.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package t3;
+
+/**
+ *
+ * @author HP
+ */
+public class T3Q3 {
+    public static void main(String[] args) {
+        //3a
+        int x=9;
+        int y=10;
+        if (x < 10)
+        if (y > 10)
+        System.out.println("*****");
+        else
+        System.out.println("#####");
+        System.out.println("$$$$$");
+        
+        //3b
+        if (x < 10) {
+        if (y < 10)
+        System.out.println("*****");
+        else{
+        System.out.println("#####");
+        System.out.println("$$$$$");
+        }}
+
+        //3c
+        if (x < 10) {
+        if (y < 10)
+        System.out.println("*****");
+        System.out.println("#####");
+        }
+        else {
+        System.out.println("$$$$$");
+        }
+        
+        //3d
+        if (x > 10) {
+        if (y > 10) {
+        System.out.println("*****");
+        System.out.println("#####"); }
+        else
+        System.out.println("$$$$$");
+        }
+
+
+    }
+}
