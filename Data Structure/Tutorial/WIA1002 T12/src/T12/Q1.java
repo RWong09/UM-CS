b5ef21b0edcd6ae9db7412e53e1e5e55d4efee04
RWong09 <147460891+RWong09@users.noreply.github.com>
2024-08-30 19:17:14 +0800

    Add files via upload

---
+++ Data Structure/Tutorial/WIA1002 T12/src/T12/Q1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package T12;
+
+public class Q1 {
+    /**
+     * List: 3 8 12 34 54 85 61 110
+     * To search 45 in the list, linear search requires to compare every element in the list 
+     * and return -1 whereas binary search requires to compare 3 elements in the list and less
+     * iterations to return -1 in the list.
+     * 
+     * To search 54 in the list, linear search requires to compare 5 elements in the list to find 
+     * the key whereas binary search requires to compare 3 elements in the list and less iterations
+     * to find the key in the list.
+     * 
+     * In short, binary search is more effective than linear search.
+     */
+}
---
+++ Data Structure/Tutorial/WIA1002 T12/src/T12/Q3Q4Q5.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package T12;
+
+/**
+ *
+ * @author HP
+ */
+public class Q3Q4Q5 {
+    /**
+     * Q3:
+     * Merge sort
+     * 
+     * Q4:
+     * Binary search
+     * 
+     * Q5:
+     * Linear search
+     */
+}
