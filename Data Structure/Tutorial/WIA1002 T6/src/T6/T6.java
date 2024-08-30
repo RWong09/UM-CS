593e7528fa164c05c3066f29035cca922f453d9c
RWong09 <147460891+RWong09@users.noreply.github.com>
2024-08-30 19:12:23 +0800

    Add files via upload

---
+++ Data Structure/Tutorial/WIA1002 T6/src/T6/T6.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package T6;
+
+/**
+ *
+ * @author HP
+ */
+public class T6 {
+    //Q1: c.peek
+    
+    /*Q2:
+    *s1:[]
+    *s2:[one,two]
+    *s3:[]
+    */
+    
+    /**Q3:
+     * (a) 1-2-3
+     * Yes.
+     * Operations: push(1) -> pop() = 1 -> push(2) -> pop() = 2 -> push(3) -> pop() = 3
+     *
+     * (b) 2-3-1
+     * Yes.
+     * Operations: push(1) -> push(2) -> pop() = 2 -> push(3) -> pop() = 3 -> pop() = 1
+     *
+     * (c) 3-2-1
+     * Yes.
+     * Operations: push(1) -> push(2) -> push(3) -> pop() = 3 -> pop() = 2 -> pop() = 1
+     *
+     * (d) 1-3-2
+     * Yes.
+     * Operations: push(1) -> pop() = 1 -> push(2) -> push(3) -> pop() = 3 -> pop() = 2
+     */
+    
+    /**Q4:
+     * a) a+b*c
+     *    abc*+
+     * b) a*b-c/d
+     *    ab*cd/-
+     * c) a+(b*c+d)/e
+     *    abc*d+e/+
+     */
+    
+    /**Q5:
+     * a) (a+b)*c
+     * b) a*(b+c)
+     */
+    
+    //Q6: D. all of the above
+}
