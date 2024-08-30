371e70c18a647117b0320223681bdf4b34e37acf
RWong09 <147460891+RWong09@users.noreply.github.com>
2024-08-30 19:17:38 +0800

    Add files via upload

---
+++ Data Structure/Tutorial/WIA1002 T13/src/T13/T13.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package T13;
+
+/**
+ *
+ * @author HP
+ */
+public class T13 {
+    /**
+     * Q1:
+     * A binary search tree is a binary tree such that for every node in the tree,
+     * the value of any node in its left subtree is less than the value of the node
+     * and the value of any node in its right subtree is greater than the value of the node.
+     * 
+     * Q2:
+     *                  50
+     *              30      71
+     *           25    40       80
+     *        1                     99
+     *          7
+     *        5
+     * 
+     * Q3:
+     * Height=5
+     * 
+     * Q4:
+     * a) Pre-order traversal:
+     *   0100 0050 0025 0001 0030 0070 0065 0085 0150 0135 0169 0180
+     * 
+     * b) In-order traversal:
+     *   0001 0025 0030 0050 0065 0070 0085 0100 0135 0150 0169 0180
+     * 
+     * c) Post-order traversal:
+     *   0001 0030 0025 0065 0085 0070 0050 0135 0180 0169 0150 0100
+     * 
+     * Q5:
+     *                                   0100
+     *                  0050                                0150
+     *          0025            0070                0135            0169
+     *  0001               0065      0085                                   0180
+     * 
+     * Q6:
+     *                                   0100
+     *                  0030                                0150
+     *          0025            0070                0135            0169
+     *  0001               0065      0085                                   0180
+     */
+}
